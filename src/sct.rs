686f12633638c19512eb3bae060e8714a371a2ab
sarah <>
2024-06-22 20:08:21 +0200

    cleanup

--- src/sct.rs
+++ src/sct.rs
@@ -4,9 +4,9 @@ use safetensors::View;
 use crate::{SignMatMut, SignMatRef};
 
 pub struct Sct {
-    pub s: Vec<u64>,
+    pub s: Box<[u64]>,
     pub c: Col<f32>,
-    pub t: Vec<u64>,
+    pub t: Box<[u64]>,
     pub nrows: usize,
     pub ncols: usize,
     pub width: usize,
@@ -16,9 +16,9 @@ pub struct Sct {
 impl Sct {
     pub fn new(nrows: usize, ncols: usize, width: usize) -> Self {
         Self {
-            s: vec![0u64; nrows.div_ceil(64) * width],
+            s: vec![0u64; nrows.div_ceil(64) * width].into_boxed_slice(),
             c: Col::zeros(width),
-            t: vec![0u64; ncols.div_ceil(64) * width],
+            t: vec![0u64; ncols.div_ceil(64) * width].into_boxed_slice(),
             nrows,
             ncols,
             width,
@@ -27,7 +27,15 @@ impl Sct {
     }
 
     pub fn as_ref(&self) -> SctRef {
-        let Self { s, c, t, nrows, ncols, width, how_full } = self;
+        let Self {
+            s,
+            c,
+            t,
+            nrows,
+            ncols,
+            width,
+            ..
+        } = self;
         SctRef {
             s: SignMatRef::from_storage(
                 crate::MatRef::from_col_major_slice(
@@ -36,9 +44,9 @@ impl Sct {
                     *width,
                     nrows.div_ceil(64),
                 ),
-                *nrows
+                *nrows,
             ),
-            c,
+            c: c.as_slice(),
             t: SignMatRef::from_storage(
                 crate::MatRef::from_col_major_slice(
                     t,
@@ -52,7 +60,15 @@ impl Sct {
     }
 
     pub fn as_mut(&mut self) -> SctMut {
-        let Self { s, c, t, nrows, ncols, width, how_full } = self;
+        let Self {
+            s,
+            c,
+            t,
+            nrows,
+            ncols,
+            width,
+            ..
+        } = self;
         SctMut {
             s: SignMatMut::from_storage(
                 crate::MatMut::from_col_major_slice(
@@ -61,9 +77,9 @@ impl Sct {
                     *width,
                     nrows.div_ceil(64),
                 ),
-                *nrows
+                *nrows,
             ),
-            c,
+            c: c.as_slice_mut(),
             t: SignMatMut::from_storage(
                 crate::MatMut::from_col_major_slice(
                     t,
@@ -79,33 +95,33 @@ impl Sct {
 
 pub struct SctRef<'a> {
     pub s: SignMatRef<'a>,
-    pub c: &'a Col<f32>,
+    pub c: &'a [f32],
     pub t: SignMatRef<'a>,
 }
 
 pub struct SctMut<'a> {
     pub s: SignMatMut<'a>,
-    pub c: &'a mut Col<f32>,
+    pub c: &'a mut [f32],
     pub t: SignMatMut<'a>,
 }
 
 impl Sct {
     pub fn views(&self) -> impl IntoIterator<Item = (String, impl View + '_)> {
-        let Self { s, c, t, nrows, ncols, width, how_full } = self;
+        let Self { s, c, t, .. } = self;
         struct DynView<'a>(Box<dyn View + 'a>);
         impl<'a> View for DynView<'a> {
             fn dtype(&self) -> safetensors::Dtype {
                 self.0.dtype()
             }
-        
+
             fn shape(&self) -> &[usize] {
                 self.0.shape()
             }
-        
+
             fn data(&self) -> std::borrow::Cow<[u8]> {
                 self.0.data()
             }
-        
+
             fn data_len(&self) -> usize {
                 self.0.data_len()
             }
@@ -114,19 +130,32 @@ impl Sct {
         trait DType {
             fn dtype() -> safetensors::Dtype;
         }
+
+        impl<'a, T> Slice<'a, T> {
+            pub fn new(slice: &'a [T]) -> Self {
+                Self(slice, slice.len())
+            }
+        }
+
+        impl<'a> DynView<'a> {
+            pub fn new(value: impl View + 'a) -> Self {
+                Self(Box::new(value))
+            }
+        }
+
         impl<'a, T: DType + bytemuck::Pod> View for Slice<'a, T> {
             fn dtype(&self) -> safetensors::Dtype {
                 T::dtype()
             }
-        
+
             fn shape(&self) -> &[usize] {
                 std::slice::from_ref(&self.1)
             }
-        
+
             fn data(&self) -> std::borrow::Cow<[u8]> {
                 bytemuck::cast_slice(self.0).into()
             }
-        
+
             fn data_len(&self) -> usize {
                 self.data().len()
             }
@@ -142,13 +171,12 @@ impl Sct {
             }
         }
         [
-            ("s".to_owned(), DynView(Box::new(Slice(s.as_slice(), s.len())))),
-            ("t".to_owned(), DynView(Box::new(Slice(t.as_slice(), t.len())))),
-            ("c".to_owned(), DynView(Box::new(Slice(c.as_slice(), c.nrows())))),
+            ("s".to_owned(), DynView::new(Slice::new(&s))),
+            ("t".to_owned(), DynView::new(Slice::new(&t))),
+            ("c".to_owned(), DynView::new(Slice::new(c.as_slice()))),
         ]
     }
 }
-
 
 #[cfg(test)]
 mod tests {
@@ -280,7 +308,7 @@ mod tests {
             iter += 1;
         }
         dbg!(iter);
-        dbg!(remainder_norm);
+        dbg!(remainder_norm / init_norm);
 
         let break_even_point = iter;
 
